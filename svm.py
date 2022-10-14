86933331e3f6e4c2bf6a2776a9dc41575721d30c
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-10-14 18:35:01 +0300

    Fit support vector machine

---
+++ svm.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.svm import SVC
+
+from ft_export import get_array_paths
+
+
+# load train dataset
+path_X, path_y = get_array_paths("train")
+X_train = np.loadtxt(path_X)
+y_train = np.loadtxt(path_y)
+
+# load dev (validation) dataset
+path_X, path_y = get_array_paths("dev")
+X_dev = np.loadtxt(path_X)
+y_dev = np.loadtxt(path_y)
+
+# fit decision tree
+clf = SVC()
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_dev)
+acc = sum(predictions == y_dev) / len(y_dev)
+print(f"SVM classification accuracy: {(acc*100):.1f}%")
