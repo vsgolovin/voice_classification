b724fda7752423aa0c1849348161de1361cf1a90
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-10-14 18:31:39 +0300

    Rename file

--- ft_export.py
+++ ft_export.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from pathlib import Path
 from tqdm import tqdm
 import numpy as np
@@ -6,16 +7,20 @@ from torchaudio import transforms as T
 from utils.dataset import load_datasets, LibriTTS
 
 
+ARRAY_PATH = Path("data/arrays")
+
+
 def main():
     train, test, dev = load_datasets(
         seed=42, transform=T.Spectrogram(n_fft=512))
-    save_to = Path("data/arrays")
+    save_to = ARRAY_PATH
     print(f"Saving Fourier transforms to {save_to}")
     for dset, name in zip((train, test, dev), ("train", "test", "dev")):
         print(f"{name} dataset:")
         X, y = dataset2arrays(dset)
-        np.savetxt(save_to / f"{name}_data.txt", X)
-        np.savetxt(save_to / f"{name}_labels.txt", y)
+        path_X, path_y = get_array_paths(name)
+        np.savetxt(path_X, X)
+        np.savetxt(path_y, y)
 
 
 def dataset2arrays(dset: LibriTTS):
@@ -24,6 +29,13 @@ def dataset2arrays(dset: LibriTTS):
         X.append(sp.squeeze(0).mean(1).numpy())
         y.append(label)
     return np.array(X, dtype=np.float64), np.array(y, dtype=np.uint8)
+
+
+def get_array_paths(subset: str = "train") -> Tuple[Path, Path]:
+    "Get paths to data and labels from selected database subset"
+    assert subset in ("train", "test", "dev"), f"Unknown split {subset}"
+    return (ARRAY_PATH / f"{subset}_data.txt",
+            ARRAY_PATH / f"{subset}_labels.txt")
 
 
 if __name__ == "__main__":
